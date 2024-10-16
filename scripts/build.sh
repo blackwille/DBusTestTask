3e976756e849a5756d28e24c6039703ed14ad330
blackwille <willem_black@vk.com>
2024-10-17 00:06:03 +0300

    changed bin directory and added build and install_deps scripts

---
+++ scripts/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# По умолчанию - папка проекта, которая на 1 уровень выше
+folder_with_cmakelists='..'
+
+# Если хотите, можно этот скрипт настроить в IDE, явно указав папку до cmakelists проекта
+if [[ $1 ]]; then 
+    folder_with_cmakelists=$1
+    echo -e "\e[0;34m""Using user specified dir with cmakelists: "$1"\e[0m"
+fi
+
+# Генерируем сборочные файлы проекта в Debug режиме 
+# и экспортируем compile_commands для адекватной работы clangd
+cmake -S${folder_with_cmakelists} -B${folder_with_cmakelists}'/build' \
+-DCMAKE_BUILD_TYPE=Debug \
+-DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+-DCMAKE_TOOLCHAIN_FILE=${folder_with_cmakelists}'/build/conan_toolchain.cmake'
+
+# Собственно, собираем сам проект
+cmake --build ${folder_with_cmakelists}'/build'
---
+++ scripts/install_deps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# По умолчанию - папка проекта, которая на 1 уровень выше
+folder_with_conanfile='..'
+
+# Если хотите, можно этот скрипт настроить в IDE, явно указав папку до conanfile проекта
+# в качестве первого аргумента
+if [[ $1 ]]; then 
+    folder_with_conanfile=$1
+    echo -e "\e[0;34m""Using user specified dir with conanfile: "$1"\e[0m"
+fi
+
+# Команда установки зависимостей с помощью пакетного манеджера 
+# (в первый раз долго из-за сборки, далее берется из кеша)
+conan install ${folder_with_conanfile} --output-folder=${folder_with_conanfile}'/build' --build=missing
